da136e3564a60a22b216bfd5aa3cc78eea905ec2
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-11-13 21:09:09 +0900

    Add Navbar bg color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
-import { Navbar } from 'react-bulma-components';
+import { Navbar, Heading } from 'react-bulma-components';
 
 export const Header: React.FC = () => {
     return (
-        <Navbar>
+        <Navbar color="success">
             <Navbar.Brand>
-                <Navbar.Item href="/">浜田市目安箱</Navbar.Item>
+                <Navbar.Item href="/">
+                    浜田市目安箱
+                </Navbar.Item>
             </Navbar.Brand>
             <Navbar.Container align="right">
                 <Navbar.Item>浜田市目安箱とは？</Navbar.Item>
