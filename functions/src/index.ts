04f45643f76a6dbe2dd50ba48eb032e66dd8c6d4
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-11-13 23:59:47 +0900

    Add sendSlack function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import {IncomingWebhook} from "@slack/webhook";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +8,25 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export const sendSlack = functions.database.ref("/opinions/{opinionId}")
+    .onCreate(async (snapshot, context) => {
+      const url = functions.config().slack.webhookurl;
+      const webhook = new IncomingWebhook(url);
+      const original = snapshot.val();
+      functions.logger.log("Uppercasing", context.params.opinionId, original);
+      functions.logger.log("Post URL", url);
+
+      let sendText = "";
+
+      sendText += `お名前: ${original.userName}\n `;
+      sendText += `メールアドレス: ${original.email}\n `;
+      sendText += `電話番号: ${original.phoneNumber}\n `;
+      sendText += `ご意見:\n\n${original.opinion}\n`;
+
+      functions.logger.log("Send text", sendText);
+
+      await webhook.send({
+        text: sendText,
+      });
+    });
