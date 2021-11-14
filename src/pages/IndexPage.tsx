08a5a4e2e6f82cb0e88ee47c7ae0b7bd6aa6048c
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-11-14 16:08:13 +0900

    Add IndexPage

---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+import { Container } from 'react-bulma-components';
+
+import { Header } from '../components/Header';
+import { AboutHero } from '../components/AboutHero';
+import { OpinionForm } from '../components/OpinionForm';
+import { Footer } from '../components/Footer';
+
+const IndexPage: React.FC = () => {
+  return (
+    <div className="App">
+      <Header />
+      <AboutHero />
+      <Container>
+        <hr />
+
+        <OpinionForm />
+
+        <hr />
+      </Container>
+      <Footer />
+    </div>
+  );
+
+}
+
+export default IndexPage;
