3f20fd1fdb6e137cda902cccabf3d8bc3e15ba66
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-11-14 15:46:57 +0900

    Add Humberger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.min.css';
 import { Navbar, Heading } from 'react-bulma-components';
 
 export const Header: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [menuClass, setMenuClass] = useState('');
+
+    const openMenu = (): void => {
+        if (isMenuOpen) {
+            setMenuClass('');
+            setIsMenuOpen(false);
+        } else {
+            setMenuClass('is-active');
+            setIsMenuOpen(true);
+        }
+    }
+
     return (
         <Navbar color="success">
             <Navbar.Brand>
                 <Navbar.Item href="/">
                     浜田市目安箱
                 </Navbar.Item>
+                <Navbar.Burger onClick={openMenu} data-target="navbarMenu" aria-label="menu" aria-expanded="false" />
             </Navbar.Brand>
+            <Navbar.Menu id="navbarMenu" className={menuClass}>
+                <Navbar.Container align="right">
+                    <Navbar.Item href="#">
+                        プライバシーポリシー
+                    </Navbar.Item>
+                </Navbar.Container>
+            </Navbar.Menu>
         </Navbar>
     );
 }
