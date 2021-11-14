caca1eda966535bff2b6e5f64ea031db971dc8a2
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-11-14 16:08:29 +0900

    Using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.min.css';
-import { Container } from 'react-bulma-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { Header } from './components/Header';
-import { AboutHero } from './components/AboutHero';
-import { OpinionForm } from './components/OpinionForm';
-import { Footer } from './components/Footer';
+import IndexPage from './pages/IndexPage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Header />
-      <AboutHero />
-      <Container>
-        <hr />
-
-        <OpinionForm />
-
-        <hr />
-      </Container>
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 
